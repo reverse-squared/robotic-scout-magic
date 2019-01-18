8d06c076ca402a25cfc44ae8c2bac652ebb6964e
Dave <imdaveead@gmail.com>
2019-01-17 19:21:41 -0500

    list format changes

--- web/components/MainPage.jsx
+++ web/components/MainPage.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
-import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import Avatar from '@material-ui/core/Avatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import { Link } from '@reach/router';
 import { hot } from 'react-hot-loader';
+
+import ExtensionIcon from '@material-ui/icons/Extension';
 
 class MainPage extends Component {
     constructor(props) {
@@ -17,7 +20,12 @@ class MainPage extends Component {
                 {
                     this.props.formData.map(item => {
                         return <ListItem key={item.id} button component={Link} to={'/form/' + item.id}>
-                            <ListItemText primary={item.name} />
+                            <ListItemAvatar>
+                                <Avatar>
+                                    <ExtensionIcon />
+                                </Avatar>
+                            </ListItemAvatar>
+                            <ListItemText primary={item.name} secondary={item.description || undefined} />
                         </ListItem>;
                     })
                 }
