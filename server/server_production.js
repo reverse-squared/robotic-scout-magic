5b0c21ee91a3294deef46d56010372317be095e7
Filip Kin <filip@kinmails.com>
2020-12-09 23:14:51 -0500

    Update server_production.js

--- server/server_production.js
+++ server/server_production.js
@@ -6,13 +6,14 @@ const api = require('./api');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
+const PORT = parseInt(process.env.PORT) || 8000;
 
 app.use(express.static(path.join(__dirname, '../dist')));
 app.use(api);
 
 io.on('connection', api.onSocket);
 
-http.listen(8000);
+http.listen(PORT);
 
 // eslint-disable-next-line no-console
-console.log('Production Server running at http://localhost:8000/');
+console.log('Production Server running at http://localhost:'+PORT);
